4b6580c17833b23e974e9941c74b6c381cd59a63
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-03-22 15:05:22 +0000

    describe header format

--- tabtool/base.py
+++ tabtool/base.py
@@ -31,8 +31,12 @@ class Field(object):
             return "{}:{}".format(self.title, self.type)
 
 
-class OrderedField(Field):
+class OrderedField(object):
+
+    """ Ordered field."""
+
     SORT_TYPES = Choices(
+        ("", "STRING", ""),
         ("M", "MONTH", "month"),
         ("R", "RANDOM", "random"),
         ("V", "VERSION", "version"),
@@ -40,14 +44,21 @@ class OrderedField(Field):
         ("h", "HUMAN_NUMERIC", "human-numeric"),
         ("n", "NUMERIC", "numeric"),
     )
+    SORT_ORDERS = Choices(
+        ("", "ASCENDING", "asc"),
+        ("r", "DESCENDING", "desc"),
+    )
 
-    def __init__(self, title, _type=None, sort_type=None, is_reverse=None):
-        super(OrderedField, self).__init__(title, _type)
+    def __init__(self, title, sort_type=None, sort_order=None):
         if sort_type is not None and sort_type not in self.SORT_TYPES:
             raise ValueError("Unknown sort type {}".format(sort_type))
 
-        self.sort_type = sort_type
-        self.is_reverse = False if is_reverse is None else bool(is_reverse)
+        if sort_order is not None and sort_order not in self.SORT_ORDERS:
+            raise ValueError("Unknown sort order {}".format(sort_order))
+
+        self.title = title
+        self.sort_type = sort_type or self.SORT_TYPES.STRING
+        self.sort_order = sort_order or self.SORT_ORDERS.ASCENDING
 
     @property
     def sort_flag(self):
@@ -55,13 +66,35 @@ class OrderedField(Field):
         if self.sort_type is not None:
             flag += self.sort_type
 
-        if self.is_reverse:
-            flag += "r"
+        if self.sort_order:
+            flag += self.sort_order
 
         return flag
 
+    def __str__(self):
+        return ":".join([self.title, self.sort_order])
+
 
 class DataDescription(object):
+
+    """ Data description, taken from header.
+
+    Data header has following format:
+
+        ^# (<FIELD>( <FIELD>)*)?(<SUBHEADER>)*(<META>)?
+
+        FIELD = ^<str>field_title(:<str>field_type)?$
+        SUBHEADER = ^ #<subheader_key>: <subheader_value>$
+        SUBHEADER:SIZE, value = size of document
+        SUBHEADER:ORDER, value = <ORDERED_FIELD>( <ORDERED_FIELD>)*
+        ORDERED_FIELD = ^<str>field_title(:sort_order)?(:sort_type)?$
+        META = ^ #META: [^\n]*
+
+    """
+
+    DELIMITERS = (",", " ", "\t")
+    DELIMITER = " "
+
     def __init__(self, fields=None, ordering=None, size=None, description=None):
         self.fields = fields or ()
         self.ordering = ordering or ()
