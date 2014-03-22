0cfe5643a2cabc62be5e0b61894adf1a0e2c4563
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-03-22 07:40:03 +0000

    init base models

---
+++ tabtool/base.py
@@ -0,0 +1,24 @@
+﻿from collections import namedtuple
+
+
+TYPES = (
+    ("str", str),
+    ("int", int),
+    ("float", float),
+    ("null", None),
+)
+
+# Sort general-numeric -g, human-numeric -h, month -M, numeric -n, random -R,
+# version -V
+
+
+class Field(namedtuple("Field", ["title", "type"])):
+    pass
+
+
+class DataDescription(object):
+    def __init__(self, fields=None, ordering=None, size=None, description=None):
+        self.fields = fields or ()
+        self.ordering = ordering or ()
+        self.size = size
+        self.description = description or ""
---
+++ tabtool/tests/test_base.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class TestField(unittest.TestCase):
+    def test_init(self):
+        pass
