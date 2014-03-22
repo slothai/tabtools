8ec3ffa96e73c798048fa505429539f25d93c4c5
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-03-22 08:52:59 +0000

    add field and ordered field classes

--- tabtool/base.py
+++ tabtool/base.py
@@ -1,19 +1,53 @@
-﻿from collections import namedtuple
+from .utils import Choices
 
 
-TYPES = (
-    ("str", str),
-    ("int", int),
-    ("float", float),
-    ("null", None),
-)
+class Field(object):
 
-# Sort general-numeric -g, human-numeric -h, month -M, numeric -n, random -R,
-# version -V
+    """ Field description."""
+
+    TYPES = Choices(
+        ("str", "STR"),
+        ("int", "INT"),
+        ("float", "FLOAT"),
+        ("null", "NULL"),
+    )
+
+    def __init__(self, title, _type=None):
+        if _type is not None and _type not in self.TYPES:
+            raise ValueError("Unknown type {}".format(_type))
+
+        self.title = title
+        self.type = _type or self.TYPES.NULL
+
+    def __repr__(self):
+        return "<{} ({}:{})>" % (
+            self.__class__.__name__, self.title, self.type
+        )
+
+    def __str__(self):
+        if self.type == self.TYPES.NULL:
+            return self.title
+        else:
+            return "{}:{}".format(self.title, self.type)
 
 
-class Field(namedtuple("Field", ["title", "type"])):
-    pass
+class OrderedField(Field):
+    SORT_TYPES = Choices(
+        ("g", "GENERAL_NUMERIC", "general-numeric"),
+        ("h", "HUMAN_NUMERIC", "human-numeric"),
+        ("M", "MONTH", "month"),
+        ("n", "NUMERIC", "numeric"),
+        ("R", "RANDOM", "random"),
+        ("V", "VERSION", "version"),
+    )
+
+    def __init__(self, title, _type=None, sort_type=None, is_reverse=None):
+        super(OrderedField, self).__init__(title, _type)
+        if sort_type is not None and sort_type not in self.SORT_TYPES:
+            raise ValueError("Unknown sort type {}".format(sort_type))
+
+        self.sort_type = sort_type
+        self.is_reverse = False if is_reverse is None else bool(is_reverse)
 
 
 class DataDescription(object):
--- tabtool/tests/test_base.py
+++ tabtool/tests/test_base.py
@@ -1,6 +1,22 @@
 import unittest
 
+from ..base import Field, OrderedField
+
 
 class TestField(unittest.TestCase):
     def test_init(self):
-        pass
+        with self.assertRaises(ValueError):
+            Field("a", "unknown_type")
+
+    def test_str_no_type(self):
+        self.assertEqual(str(Field("a")), "a")
+        self.assertEqual(str(Field("a", None)), "a")
+
+    def test_str_with_type(self):
+        self.assertEqual(str(Field("a", "str")), "a:str")
+
+
+class TestOrderedField(unittest.TestCase):
+    def test_init(self):
+        with self.assertRaises(ValueError):
+            OrderedField("a", sort_type="unknown sort type")
