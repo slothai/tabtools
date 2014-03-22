3a6c195f8a93daa6d34969f473f461652080dfba
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-03-22 09:03:11 +0000

    add unix sort flag generation

--- tabtool/base.py
+++ tabtool/base.py
@@ -33,12 +33,12 @@ class Field(object):
 
 class OrderedField(Field):
     SORT_TYPES = Choices(
-        ("g", "GENERAL_NUMERIC", "general-numeric"),
-        ("h", "HUMAN_NUMERIC", "human-numeric"),
         ("M", "MONTH", "month"),
-        ("n", "NUMERIC", "numeric"),
         ("R", "RANDOM", "random"),
         ("V", "VERSION", "version"),
+        ("g", "GENERAL_NUMERIC", "general-numeric"),
+        ("h", "HUMAN_NUMERIC", "human-numeric"),
+        ("n", "NUMERIC", "numeric"),
     )
 
     def __init__(self, title, _type=None, sort_type=None, is_reverse=None):
@@ -48,6 +48,17 @@ class OrderedField(Field):
 
         self.sort_type = sort_type
         self.is_reverse = False if is_reverse is None else bool(is_reverse)
+
+    @property
+    def sort_flag(self):
+        flag = ""
+        if self.sort_type is not None:
+            flag += self.sort_type
+
+        if self.is_reverse:
+            flag += "r"
+
+        return flag
 
 
 class DataDescription(object):
--- tabtool/tests/test_base.py
+++ tabtool/tests/test_base.py
@@ -20,3 +20,26 @@ class TestOrderedField(unittest.TestCase):
     def test_init(self):
         with self.assertRaises(ValueError):
             OrderedField("a", sort_type="unknown sort type")
+
+    def test_sort_flag(self):
+        self.assertEqual(OrderedField("a").sort_flag, "")
+        self.assertEqual(OrderedField("a", sort_type="M").sort_flag, "M")
+        self.assertEqual(OrderedField("a", sort_type="R").sort_flag, "R")
+        self.assertEqual(OrderedField("a", sort_type="V").sort_flag, "V")
+        self.assertEqual(OrderedField("a", sort_type="g").sort_flag, "g")
+        self.assertEqual(OrderedField("a", sort_type="h").sort_flag, "h")
+        self.assertEqual(OrderedField("a", sort_type="n").sort_flag, "n")
+
+        self.assertEqual(OrderedField("a", is_reverse=True).sort_flag, "r")
+        self.assertEqual(
+            OrderedField("a", sort_type="M", is_reverse=True).sort_flag, "Mr")
+        self.assertEqual(
+            OrderedField("a", sort_type="R", is_reverse=True).sort_flag, "Rr")
+        self.assertEqual(
+            OrderedField("a", sort_type="V", is_reverse=True).sort_flag, "Vr")
+        self.assertEqual(
+            OrderedField("a", sort_type="g", is_reverse=True).sort_flag, "gr")
+        self.assertEqual(
+            OrderedField("a", sort_type="h", is_reverse=True).sort_flag, "hr")
+        self.assertEqual(
+            OrderedField("a", sort_type="n", is_reverse=True).sort_flag, "nr")
