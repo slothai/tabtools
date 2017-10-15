d3cb6652f3a790e69744c524e2dfd18f8cbeae7d
Kirill Pavlov <k@p99.io>
2017-10-15 22:53:09 +0800

    DEL: remove unsupported functionality tests

--- tabtools/tests/test_files.py
+++ tabtools/tests/test_files.py
@@ -1,15 +1,20 @@
 import unittest
+import sys
 
-from ..files import File, RegularFile
+try:
+    from unittest.mock import patch
+    from io import StringIO
+except ImportError:
+    from mock import patch
+    from StringIO import StringIO
+
+from ..files import File, StreamFile, RegularFile
 
 
 class TestFile(unittest.TestCase):
     def setUp(self):
         self.fd = open('tabtools/tests/files/sample1.tsv')
         self.f = File(self.fd)
-
-    def test_descriptor(self):
-        self.assertTrue(self.f.descriptor.startswith("/dev/fd/"))
 
     def test_proxy(self):
         self.assertIsInstance(self.f.proxy, RegularFile)
