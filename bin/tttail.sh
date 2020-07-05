73e8e9b8b149c5cbcc31e9fd8d81518db1b3835a
Kirill Pavlov <k@p99.io>
2020-07-05 12:39:20 +0800

    REF: change from file descriptors to named pipes

--- bin/tttail.sh
+++ bin/tttail.sh
@@ -1,3 +1,9 @@
 # tee >(head -n1) >/dev/null | >(tail "$@")
 # Reference implementation: https://unix.stackexchange.com/questions/28503/how-can-i-send-stdout-to-multiple-commands
-{ { tee /dev/fd/3 | head -n1 >&9; } 3>&1 | tail -qn+2 | tail "$@" >&9; } 9>&1
+# { { tee /dev/fd/3 | head -n1 >&9; } 3>&1 | tail -qn+2 | tail "$@" >&9; } 9>&1
+
+tmp=$(mktemp)
+cat > $tmp
+head -n1 $tmp
+tail -qn+2 $tmp | tail "$@"
+rm -rf "$tmp"
