3e097433374a6ddda42dc062a6808d602baed1b0
Kirill Pavlov <k@p99.io>
2022-07-30 13:23:55 +0100

    TST: migrate tests from pytest to unittest

--- tabtools/tests/test_base.py
+++ tabtools/tests/test_base.py
@@ -1,78 +1,78 @@
-import pytest
+import unittest
 
 from ..base import Field, Header, Subheader, SubheaderCount
 
 
-class TestField:
+class TestField(unittest.TestCase):
     def test_init(self):
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             Field("")
 
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             Field("a", "unknown_type")
 
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             Field("title with space")
 
     def test_str_no_type(self):
-        assert str(Field("a")) == "a"
-        assert str(Field("a", None)) ==  "a"
+        self.assertEqual(str(Field("a")), "a")
+        self.assertEqual(str(Field("a", None)),  "a")
 
     def test_str_with_type(self):
-        assert str(Field("a", Field.TYPES.STRING)) == "a:str"
+        self.assertEqual(str(Field("a", Field.TYPES.STRING)), "a:str")
 
     def test__eq__(self):
-        assert Field("a") == Field("a")
-        assert Field("a") == Field("a", None)
-        assert Field("a", Field.TYPES.STRING) == Field("a", Field.TYPES.STRING)
+        self.assertEqual(Field("a"), Field("a"))
+        self.assertEqual(Field("a"), Field("a", None))
+        self.assertEqual(Field("a", Field.TYPES.STRING), Field("a", Field.TYPES.STRING))
 
-        assert Field("a") != Field("b")
-        assert Field("a") != Field("a", Field.TYPES.STRING)
-        assert Field("a", Field.TYPES.NUMBER) != Field("a", Field.TYPES.STRING)
+        self.assertNotEqual(Field("a"), Field("b"))
+        self.assertNotEqual(Field("a"), Field("a", Field.TYPES.STRING))
+        self.assertNotEqual(Field("a", Field.TYPES.NUMBER), Field("a", Field.TYPES.STRING))
 
     def test_parse(self):
-        assert Field.parse("a") == Field("a")
-        assert Field.parse("a:str") == Field("a", Field.TYPES.STRING)
+        self.assertEqual(Field.parse("a"), Field("a"))
+        self.assertEqual(Field.parse("a:str"), Field("a", Field.TYPES.STRING))
 
     def test_parse_error(self):
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             Field.parse("a:")  # type is missing
 
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             Field.parse("a str")  # space in field title
 
     def test_union_simple(self):
-        assert Field.union(Field("a")) ==  Field("a")
-        assert Field.union(Field("a"), Field("a")) == Field("a")
+        self.assertEqual(Field.union(Field("a")),  Field("a"))
+        self.assertEqual(Field.union(Field("a"), Field("a")), Field("a"))
 
     def test_union_different_names(self):
-        assert Field.union(Field("a"), Field("b")) == Field("a")
-        assert Field.union(Field("b"), Field("a")) == Field("b")
+        self.assertEqual(Field.union(Field("a"), Field("b")), Field("a"))
+        self.assertEqual(Field.union(Field("b"), Field("a")), Field("b"))
 
     def test_union_same_type(self):
-        assert Field.union(Field("a"), Field("a")) == Field("a")
+        self.assertEqual(Field.union(Field("a"), Field("a")), Field("a"))
 
         for t in Field.TYPES:
-            assert Field.union(Field("a", t), Field("a", t)) == Field("a", t)
+            self.assertEqual(Field.union(Field("a", t), Field("a", t)), Field("a", t))
 
     def test_union_different_types(self):
-        assert Field.union(Field("a", Field.TYPES.STRING), Field("a")) == Field("a")
-        assert Field.union(Field("a", Field.TYPES.NUMBER), Field("a")) == Field("a")
-        assert Field.union(Field("a", Field.TYPES.STRING), Field("a", Field.TYPES.NUMBER)) == Field("a", Field.TYPES.STRING)
+        self.assertEqual(Field.union(Field("a", Field.TYPES.STRING), Field("a")), Field("a"))
+        self.assertEqual(Field.union(Field("a", Field.TYPES.NUMBER), Field("a")), Field("a"))
+        self.assertEqual(Field.union(Field("a", Field.TYPES.STRING), Field("a", Field.TYPES.NUMBER)), Field("a", Field.TYPES.STRING))
 
     def test_union_error(self):
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             Field.union()
 
 
-class TestSubheader:
+class TestSubheader(unittest.TestCase):
     def test_str(self):
-        assert str(Subheader("key", "value")) == "KEY:value"
-        assert str(Subheader("order", "a:asc b:desc")) == "ORDER:a:asc b:desc"
+        self.assertEqual(str(Subheader("key", "value")), "KEY:value")
+        self.assertEqual(str(Subheader("order", "a:asc b:desc")), "ORDER:a:asc b:desc")
 
     def test__eq__(self):
-        assert Subheader("key", "value") == Subheader("KEY", "value")
-        assert Subheader("key", "value") != Subheader("order", "a:asc b:desc")
+        self.assertEqual(Subheader("key", "value"), Subheader("KEY", "value"))
+        self.assertNotEqual(("key", "value"), Subheader("order", "a:asc b:desc"))
 
     def test__eq__inherited(self):
         class SubheaderChild(Subheader):
@@ -81,52 +81,52 @@ class TestSubheader:
         subheader_child1 = SubheaderChild("key", "value")
         subheader_child2 = SubheaderChild("key", "value")
 
-        assert subheader_child1 == subheader_child2
-        assert Subheader("key", "value") != subheader_child1
+        self.assertEqual(subheader_child1, subheader_child2)
+        self.assertNotEqual(Subheader("key", "value"), subheader_child1)
 
     def test_parse(self):
-        assert Subheader.parse("key:value") == Subheader("key", "value")
-        assert Subheader.parse("ORDER:a:asc b:desc") == Subheader("order", "a:asc b:desc")
+        self.assertEqual(Subheader.parse("key:value"), Subheader("key", "value"))
+        self.assertEqual(Subheader.parse("ORDER:a:asc b:desc"), Subheader("order", "a:asc b:desc"))
 
     def test_parse_incorrect(self):
         Subheader.parse("key:value")
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             Subheader.parse("key value")
 
     def test_union(self):
-        assert Subheader.union(
+        self.assertEqual(Subheader.union(
             Subheader("k", "v1"),
             Subheader("k", "v2")
-        ) == Subheader("k", "?")
+        ), Subheader("k", "?"))
 
     def test_union_error(self):
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             Subheader.union()
 
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             Subheader.union(
                 Subheader("k1", "v"),
                 Subheader("k2", "v")
             )
 
 
-class TestSubheaderCount:
+class TestSubheaderCount(unittest.TestCase):
     def test_init_value(self):
         subheader1 = SubheaderCount("count", 1)
         subheader2 = SubheaderCount("count", "1")
-        assert subheader1.value == 1
-        assert subheader2.value == 1
-        assert subheader1 == subheader2
+        self.assertEqual(subheader1.value, 1)
+        self.assertEqual(subheader2.value, 1)
+        self.assertEqual(subheader1, subheader2)
 
     def test_merge(self):
-        assert SubheaderCount.union(
+        self.assertEqual(SubheaderCount.union(
             SubheaderCount("count", 1),
             SubheaderCount("count", 2)
-        ) == SubheaderCount("count", 3)
+        ), SubheaderCount("count", 3))
 
 
-class TestHeader:
-    def setup_method(self, method):
+class TestHeader(unittest.TestCase):
+    def setUp(self):
         self.fields = (
             Field("a", Field.TYPES.NUMBER),
             Field("b", Field.TYPES.STRING),
@@ -144,10 +144,10 @@ class TestHeader:
             subheaders=self.subheaders,
         )
 
-        assert str(header) == "a:num\tb:str\tc #COUNT:1 #ORDER:a:asc"
+        self.assertEqual(str(header), "a:num\tb:str\tc #COUNT:1 #ORDER:a:asc")
 
         header.delimiter = ','
-        assert str(header) == "a:num,b:str,c #COUNT:1 #ORDER:a:asc"
+        self.assertEqual(str(header), "a:num,b:str,c #COUNT:1 #ORDER:a:asc")
 
     def test__eq__fields(self):
         header1 = Header(
@@ -160,13 +160,13 @@ class TestHeader:
             fields=self.fields,
             subheaders=self.subheaders,
         )
-        assert header1 == header2
+        self.assertEqual(header1, header2)
 
         header2.fields=self.fields[1:]
-        assert header1 != header2
+        self.assertNotEqual(header1, header2)
 
         header2.fields=self.fields[1:2] + self.fields[0:1] + self.fields[2:]
-        assert header1 != header2
+        self.assertNotEqual(header1, header2)
 
     def test__eq__subheaders(self):
         header1 = Header(
@@ -180,32 +180,32 @@ class TestHeader:
             subheaders=self.subheaders[1:],
         )
 
-        assert header1 != header2
+        self.assertNotEqual(header1, header2)
 
         header2.subheaders=self.subheaders[1:] + self.subheaders[0:1]
-        assert header1 == header2
+        self.assertEqual(header1, header2)
 
     def test_generate(self):
-        assert "f0,f1,f2" == str(Header.generate(',', 3))
+        self.assertEqual("f0,f1,f2", str(Header.generate(',', 3)))
 
     def test_parse(self):
-        assert Header.parse("a:num\tb:str\tc #COUNT:1 #ORDER:a:asc") == Header(
+        self.assertEqual(Header.parse("a:num\tb:str\tc #COUNT:1 #ORDER:a:asc"), Header(
             delimiter='\t',
             fields=self.fields,
             subheaders=self.subheaders
-        )
+        ))
         
-        assert Header.parse("a:num,b:str,c #COUNT:1 #ORDER:a:asc") == Header(
+        self.assertEqual(Header.parse("a:num,b:str,c #COUNT:1 #ORDER:a:asc"), Header(
             delimiter=',',
             fields=self.fields,
             subheaders=self.subheaders
-        )
+        ))
 
-        assert Header.parse("a:num|b:str|c #COUNT:1 #ORDER:a:asc", delimiter="|") == Header(
+        self.assertEqual(Header.parse("a:num|b:str|c #COUNT:1 #ORDER:a:asc", delimiter="|"), Header(
             delimiter='|',
             fields=self.fields,
             subheaders=self.subheaders
-        )
+        ))
 
     def test_union(self):
         h1 = Header(
@@ -241,4 +241,8 @@ class TestHeader:
                 SubheaderCount("COUNT", 2),
             ),
         )
-        assert Header.union(h1, h2) == expected
+        self.assertEqual(Header.union(h1, h2), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tabtools/tests/test_files.py
+++ tabtools/tests/test_files.py
@@ -1,8 +1,9 @@
+import unittest
 from ..files import File, RegularFile
 
 
-class TestFile:
+class TestFile(unittest.TestCase):
     def test_proxy(self):
         with open('tabtools/tests/files/sample1.tsv') as fd:
             f = File(fd, has_header=True)
-            assert isinstance(f.proxy, RegularFile)
+            self.assertTrue(isinstance(f.proxy, RegularFile))
